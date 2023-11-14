b4da608140b8b7b5415b6a69fad509b726191bab
LEUNG KAM YING <lkyvcvc@gmail.com>
2023-11-14 11:25:49 +0900

    validation and association done

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -1,18 +1,12 @@
 class Cup < ApplicationRecord
 
   belongs_to :user
-
   has_many  :bookings
 
   validates :name, presence: true, length: { minimum: 6 }
-  validates :description, presence: true, length: { minimum: 25 }
-
+  validates :type, presence: true, inclusion: ["bottle", "cup", "thermos", "mug"]
   validates :location, presence: true
-
-  validates :price, presence: true, numericality: { only_integer: true }
-
   validates :volume, presence: true, numericality: { only_integer: true }
-
-  validates :type, presence: true, inclusion: ["bottle","cup", "thermos","mug"]
-
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :price, presence: true, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :cups
+  has_many :bookings
   has_many :bookings_as_owner, through: :cups, source: :bookings
 
   validates :phone_number, presence: true, length: { minimum: 7 }, format: { with: /\A\d+\z/, message: "Number only." }
