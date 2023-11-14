28acc06fb5a41a6e32689e5b96df280b3f323955
Michael Sam <sam.michael.h@gmail.com>
2023-11-14 16:12:24 +0900

    Create a cup form

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -3,8 +3,10 @@ class Cup < ApplicationRecord
   belongs_to :user
   has_many  :bookings
 
+  TYPE = ["bottle", "cup", "thermos", "mug"]
+
   validates :name, presence: true, length: { minimum: 6 }
-  validates :type, presence: true, inclusion: ["bottle", "cup", "thermos", "mug"]
+  validates :type, presence: true, inclusion: TYPE
   validates :location, presence: true
   validates :volume, presence: true, numericality: { only_integer: true }
   validates :description, presence: true, length: { minimum: 10 }
