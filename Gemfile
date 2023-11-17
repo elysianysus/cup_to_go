cf3cf4a51906c8b0a29ffa82e9c1be4268a48ea2
Michael Sam <sam.michael.h@gmail.com>
2023-11-17 14:58:27 +0900

    Create map for cup listing

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ end
 gem "devise"
 
 gem 'cloudinary'
+
+gem "geocoder"
--- app/models/cup.rb
+++ app/models/cup.rb
@@ -2,6 +2,7 @@ class Cup < ApplicationRecord
 
   belongs_to :user
   has_many  :bookings
+  geocoded_by :location
 
   CATEGORY = ["bottle", "cup", "thermos", "mug"]
 
@@ -11,6 +12,7 @@ class Cup < ApplicationRecord
   validates :volume, presence: true, numericality: { only_integer: true }
   validates :description, presence: true, length: { minimum: 10 }
   validates :price, presence: true, numericality: { only_integer: true }
+  after_validation :geocode, if: :will_save_change_to_location?
 
   has_one_attached :photo
 
